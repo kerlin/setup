becf3537c10a849eb9dd66fcc13de20a232f776b
Chris Kerlin <chris.kerlin@gmail.com>
2016-06-08 14:47:34 -0400

    Update run_setup.sh
    tree

--- run_setup.sh
+++ run_setup.sh
@@ -1,6 +1,7 @@
 # curl -L https://raw.github.com/kerlin/setup/my_setup/run_setup.sh | sh
 cd $HOME
 sudo apt-get update
+sudo apt-get install -y tree
 sudo apt-get install -y git-core
 git clone https://github.com/kerlin/setup.git
 ./setup/setup.sh
